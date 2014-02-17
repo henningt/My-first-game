de3d5f2a3de37e17dbda1121ad22bcba15440a53
Henning <tuiningah@gmail.com>
2014-02-17 14:41:21 -0500

    wrote the first version of my program

--- My-first-game.py
+++ My-first-game.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
+
 from random import choice 
+
 # choose a number
+secret_number = 5
+
 # ask user to guess a number 
+some_number = raw_input() 
+
 # compare the number that they guessed to the random number 
-# compare results and display 
+if secret_number == int(some_number):
+	print "good job ps you still suck"
+else:
+	print " you are bad at life"
